ee23e84ee814abcd30b1403b42608fde866590fa
zoeyzou <13701719+zoeyzou@users.noreply.github.com>
2018-12-02 22:08:41 +0100

    fix: fix linting error and add axios to replace native fetch api

---
+++ src/client/api/api.js
@@ -0,0 +1,6 @@
+const axios = require("axios");
+axios.defaults.baseURL = "http://localhost:5000/";
+
+export function fetchMentor() {
+  return axios.get("/");
+}
--- src/server/index.js
+++ src/server/index.js
@@ -2,13 +2,15 @@ const fs = require("fs");
 const path = require("path");
 
 const express = require("express");
+const cors = require("cors");
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
+// eslint-disable-next-line no-undef
 const filePath = path.join(__dirname, "db.json");
 
 const testData = JSON.parse(fs.readFileSync(filePath, "utf8"));
 
-app.get("/", (req, res) => res.send(testData));
+app.get("/", cors(), (req, res) => res.send(testData));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
