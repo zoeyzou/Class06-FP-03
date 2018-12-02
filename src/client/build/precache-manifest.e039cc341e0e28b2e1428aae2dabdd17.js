13829ffb97cd52d10c2406f490e8c5f3fbd5f9be
zoeyzou <13701719+zoeyzou@users.noreply.github.com>
2018-12-02 22:14:26 +0100

    Adding `build` to source control

---
+++ src/client/build/precache-manifest.e039cc341e0e28b2e1428aae2dabdd17.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e9a17d501893ba7785d8",
+    "url": "/static/js/main.e9a17d50.chunk.js"
+  },
+  {
+    "revision": "1f6e8cd0ae4db3ab0857",
+    "url": "/static/js/1.1f6e8cd0.chunk.js"
+  },
+  {
+    "revision": "e9a17d501893ba7785d8",
+    "url": "/static/css/main.e1f49dc0.chunk.css"
+  },
+  {
+    "revision": "9091a61209eb89056129aa9f371e1f92",
+    "url": "/index.html"
+  }
+];
