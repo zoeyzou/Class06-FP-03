3fabff4660ac1102dfd106e8e7c980f476ae8652
zoeyzou <13701719+zoeyzou@users.noreply.github.com>
2018-12-05 21:19:42 +0100

    fix: add build path and let backend serve static file

--- src/server/index.js
+++ src/server/index.js
@@ -6,11 +6,18 @@ const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
 
+const buildPath = path.join(__dirname, "../../build");
+app.use(express.static(buildPath));
+
 // eslint-disable-next-line no-undef
 const filePath = path.join(__dirname, "db.json");
 
 const testData = JSON.parse(fs.readFileSync(filePath, "utf8"));
 
+app.get("/*", (req, res) => {
+  const indexPath = path.join(buildPath, "index.html");
+  res.sendFile(indexPath);
+});
 app.get("/api/mentors", cors(), (req, res) => res.send(testData));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
