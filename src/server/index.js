384d83bfdf1f3e2cc04d6c91997870b12d08131a
zoeyzou <13701719+zoeyzou@users.noreply.github.com>
2018-12-06 22:47:17 +0100

    fix: added a static route to access static file

--- src/server/index.js
+++ src/server/index.js
@@ -5,9 +5,11 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
-
 const buildPath = path.join(__dirname, "../../build");
-app.use(express.static(buildPath));
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(buildPath));
+}
 
 // eslint-disable-next-line no-undef
 const filePath = path.join(__dirname, "db.json");
@@ -15,6 +17,11 @@ const filePath = path.join(__dirname, "db.json");
 const testData = JSON.parse(fs.readFileSync(filePath, "utf8"));
 
 app.get("/api/mentors", cors(), (req, res) => res.send(testData));
+
+app.get("/static/*", (req, res) => {
+  const fullFilePath = path.join(buildPath, req.originalUrl);
+  res.sendFile(fullFilePath); // SECURITY ISSUE!
+});
 
 app.get("/*", (req, res) => {
   const indexPath = path.join(buildPath, "index.html");
