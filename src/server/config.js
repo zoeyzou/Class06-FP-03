fabe58d643a4d393986838163774c290e6be5172
nirmala <nirmala@surf.net.np>
2018-12-08 20:10:35 +0100

    use dotenv for local env

--- src/server/config.js
+++ src/server/config.js
@@ -1,3 +1,6 @@
+const path = require('path');
+require('dotenv').config({ path: path.resolve(process.cwd(), '.env.development.server') })
+
 const config = {
   host: process.env.host,
   user: process.env.user,
