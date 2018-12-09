b51f2b76e2d0741ccb84d2fe2fc72cab1ed34dbc
nirmala <nirmala@surf.net.np>
2018-12-09 09:22:36 +0100

    add input and button for test

--- src/client/App.js
+++ src/client/App.js
@@ -18,17 +18,21 @@ class App extends Component {
 
     return (
       <div className="App">
-        <ul>
-          {mentors &&
-            mentors.map(mentor => (
-              <li key={mentor.id}>
-                {mentor.first_name} {mentor.last_name}{" "}
-                <a href={mentor.Slack_nickname}>{mentor.Slack_nickname}</a>
-              </li>
-            ))}
-        </ul>
-        <input type="text" value="" />
-        <button>add</button>
+        <div className="container">
+          <ul>
+            {mentors &&
+              mentors.map(mentor => (
+                <li key={mentor.id}>
+                  {mentor.first_name} {mentor.last_name}{" "}
+                  <a href={mentor.Slack_nickname}>{mentor.Slack_nickname}</a>
+                </li>
+              ))}
+          </ul>
+          <input type="text" value="" />
+          <button>add</button>
+          <button>Edit</button>
+          <button>Delete</button>
+        </div>
       </div>
     );
   }
