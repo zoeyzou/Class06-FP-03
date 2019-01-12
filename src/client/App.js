ce60945019bd741ac6d1f0c79266356e295f370c
nirmala <nirmala@surf.net.np>
2019-01-12 18:47:30 +0100

    remove old code and add signUp form

--- src/client/App.js
+++ src/client/App.js
@@ -1,27 +1,11 @@
 import React, { Component } from "react";
-import { fetchMentor } from "./api/api";
+import SignUp from "./SignUp";
 
 class App extends Component {
-  state = {
-    mentors: null,
-  };
-  componentDidMount() {
-    fetchMentor().then(response => this.setState({ mentors: response.data }));
-  }
   render() {
-    const { mentors } = this.state;
-
     return (
       <div className="App">
-        <ul>
-          {mentors &&
-            mentors.map(mentor => (
-              <li key={mentor.id}>
-                {mentor.first_name} {mentor.last_name}{" "}
-                <a href={mentor.Slack_nickname}>{mentor.Slack_nickname}</a>
-              </li>
-            ))}
-        </ul>
+        <SignUp />
       </div>
     );
   }
