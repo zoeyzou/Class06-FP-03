681cc7fd7cfda4b7aa03ca7a5e34c0df742a3c3a
nirmala <nirmala@surf.net.np>
2019-01-13 12:22:21 +0100

    add router in app

--- src/client/App.js
+++ src/client/App.js
@@ -1,14 +1,17 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
 import SignUp from "./SignUp";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="wrapper">
+      <Router>
+        <div>
           <SignUp />
+          <Route path="/" Component={SignUp} />
         </div>
-      </div>
+      </Router>
     );
   }
 }
