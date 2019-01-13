a3a055a122c41d1fd44bd03e3646f1f06a749089
nirmala <nirmala@surf.net.np>
2019-01-13 12:23:06 +0100

    make post to database and create user

--- src/client/SignUp.js
+++ src/client/SignUp.js
@@ -1,4 +1,5 @@
 import React from "react";
+import axios from "axios";
 
 class SignUp extends React.Component {
   constructor(props) {
@@ -7,35 +8,45 @@ class SignUp extends React.Component {
       name: "",
       email: "",
       password: "",
+      message: "",
     };
   }
   handleChange = () => {
     this.setState({
+      name: event.target.value,
       email: event.target.value,
       password: event.target.value,
     });
   };
   handleSubmit = e => {
     e.preventDefault();
-    console.log("form submit");
+    const { name, email, password } = this.state;
+    if (name === "" && email === "" && password === "") {
+      return;
+    } else {
+      axios.post("/users", { name, email, password }).then(result => {
+        console.log(result, "user created");
+        this.setState({ isSubmitted: true });
+      });
+    }
   };
+
   render() {
     return (
-      <div>
+      <div className="formWrapper">
         <form onSubmit={this.handleSubmit}>
           <label>
-            Name:
+            Name
             <input type="text" placeholder="Enter Name" onChange={this.handleChange} />
           </label>
           <label>
-            Email:
+            Email
             <input type="email" placeholder="Enter Email" onChange={this.handleChange} />
           </label>
           <label>
-            Password:
+            Password
             <input type="password" placeholder="Enter password" onChange={this.handleChange} />
           </label>
-
           <button type="submit">SignUp</button>
         </form>
       </div>
